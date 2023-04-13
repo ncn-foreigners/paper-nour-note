7ba7c2f1873cb1b6d9d992055b4539ed7d754833
Maciej <maciej.beresewicz@gmail.com>
2023-04-13 12:04:49 +0200

    calculations and changes

--- README.md
+++ README.md
@@ -7,8 +7,16 @@ Repository for the paper "A note on Nour (1982) dual-system estimator"
 1. Install Julia [download](https://julialang.org/downloads/)
 2. Clone repository with `git clone https://github.com/ncn-foreigners/paper-nour-note.git`
 3. Run `cd paper-nour-note`
-4. Run `julia --project=. -e "using Pkg; Pkg.instantiate()"`
-5. Run `julia --project=. -e "using IJulia; notebook(dir=pwd())"`
+4. Run `julia` in this folder (you may check the working directory using `pwd()`)
+5. Run the following code
+
+```julia
+using Pkg
+Pkg.activate(".")
+
+using IJulia
+notebook(dir=".")
+```
 
 
 
@@ -28,6 +36,7 @@ Packages
   [a93c6f00] DataFrames v1.5.0
   [31c24e10] Distributions v0.25.87
   [7073ff75] IJulia v1.24.0
+  [23fbe1c1] Latexify v0.15.18
   [91a5bcdd] Plots v1.38.9
   [9a3f8284] Random
   [10745b16] Statistics v1.9.0
--- codes/functions.jl
+++ codes/functions.jl
@@ -23,3 +23,173 @@ function generate_counts(
     
     return res
 end
+
+function bias_approximation(;N::Integer, α::Real, 
+                            p₁::Real, p₂::Real, 
+                            dependence = "+", 
+                            bias_type = "Nour")
+    if dependence == "+"
+        p₀₀ = α * (1 - p₁) + (1 - α) * (1 - p₁)* (1 - p₂)
+        p₁₀ = (1 - α) * p₁ * (1 - p₂)
+        p₀₁ = (1 - α) * (1 - p₁) * p₂
+        p₁₁ = α * p₁ + (1 - α) * p₁ * p₂
+    elseif dependence == "-"
+        p₀₀ = (1 - α) * (1 - p₁) * (1 - p₂)
+        p₁₀ = α * p₁ + (1 - α) * p₁ * (1 - p₂)
+        p₀₁ = α * (1 - p₁) + (1 - α) * (1 - p₁) * p₂
+        p₁₁ = (1 - α) * p₁ * p₂
+    end #end if
+    
+    # These expressions do not change when dependence type change **
+    # only inclusion probablities change.
+    if bias_type == "Nour"
+        bias_lower = N * (- p₁₁ * (p₁₁ * p₀₀ - p₁₀ * p₀₁) + (p₁₁ - p₀₀) * p₁₀ * p₀₁) / (p₁₁ ^ 2 + p₁₀ * p₀₁)
+        bias_mean  = "Not claculated"
+        bias_upper = "Not claculated"
+    else 
+        bias_upper  = N * (p₁₁ + p₁₀ + p₀₁ + sqrt(p₁₀ * p₀₁) - 1)
+        bias_upper -= (sqrt(p₁₀ * p₀₁) / 2 + sqrt(p₀₁ / p₁₀) * (1 - p₁₀)/ 4 + sqrt(p₁₀ / p₀₁) * (1 - p₀₁)/ 4) / 2
+        # constant term
+        bias_lower  = (p₁₁ + p₁₀ + p₀₁ + 2 * p₁₁ * p₁₀ * p₀₁ / (p₁₁ ^ 2 + p₁₀ * p₀₁) - 1) * N
+        # variance terms of order N^-2
+        bias_lower += 2 * (p₁₁ * (1 - p₁₁) * p₁₁ * p₁₀ * p₀₁ * 
+        ((p₁₁ ^ 2 - 3 * p₁₀ * p₀₁) / ((p₁₁ ^ 2 + p₁₀ * p₀₁) ^ 4))
+        - p₁₀ * (1 - p₁₀) * (p₁₁ ^ 3) * (p₀₁ ^ 2) / ((p₁₁ ^ 2 + p₁₀ * p₀₁) ^ 4)
+        - p₀₁ * (1 - p₀₁) * (p₁₁ ^ 3) * (p₁₀ ^ 2) / ((p₁₁ ^ 2 + p₁₀ * p₀₁) ^ 4)) / (N ^ 2)
+        # covariance terms of order N^0
+        bias_lower -= 2 * (p₁₁ * p₁₀ * (3 * (p₁₁ ^ 2) * p₀₁ * ((p₁₁ ^ 2 + p₁₀ * p₀₁) ^ 2) - 
+        4 * (p₁₁ ^ 4) * p₀₁ * (p₁₁ ^ 2 + p₁₀ * p₀₁)) + 
+        p₁₁ * p₀₁ * (3 * (p₁₁ ^ 2) * p₁₀ * ((p₁₁ ^ 2 + p₁₀ * p₀₁) ^ 2) - 
+        4 * (p₁₁ ^ 4) * p₁₀ * ((p₁₁ ^ 2) + p₁₀ * p₀₁)) + 
+        p₁₀ * p₀₁ * (p₁₁ ^ 3) * (p₁₁ ^ 3 - p₁₀ * p₀₁) * (p₁₁ ^ 2 + p₁₀ * p₀₁)) / ((p₁₁ ^ 2 + p₁₀ * p₀₁) ^ 4)
+        bias_mean = (bias_upper + bias_lower) / 2 # thankfully this is linear
+    end #end if
+    
+    return (bias_lower, bias_mean, bias_upper)
+end
+
+
+
+function variance_approximation(;N::Integer, α::Real, 
+                                 p₁::Real, p₂::Real, 
+                                 dependence = "+")
+    if dependence == "+"
+        p₀₀ = α * (1 - p₁) + (1 - α) * (1 - p₁)* (1 - p₂)
+        p₁₀ = (1 - α) * p₁ * (1 - p₂)
+        p₀₁ = (1 - α) * (1 - p₁) * p₂
+        p₁₁ = α * p₁ + (1 - α) * p₁ * p₂
+    elseif dependence == "-"
+        p₀₀ = (1 - α) * (1 - p₁) * (1 - p₂)
+        p₁₀ = α * p₁ + (1 - α) * p₁ * (1 - p₂)
+        p₀₁ = α * (1 - p₁) + (1 - α) * (1 - p₁) * p₂
+        p₁₁ = (1 - α) * p₁ * p₂
+    end #end if
+    
+    # These expressions do not change when dependence type changes **
+    # only inclusion probablities change.
+    
+    # this is from variance terms
+    var_upper  = p₁₁ * (1 - p₁₁) + p₀₁ * (1 - p₀₁) + p₁₀ * (1 - p₁₀)
+    var_upper += p₁₀ * (1 - p₁₀) * ((p₀₁ / p₁₀) ^ .5 + (p₀₁ / p₁₀) / 4)
+    var_upper += p₀₁ * (1 - p₀₁) * ((p₁₀ / p₀₁) ^ .5 + (p₁₀ / p₀₁) / 4)
+    # this is from covariance temrs
+    var_upper -= (2 * p₁₁ * p₀₁ * (1 + ((p₁₀ / p₀₁) ^ .5) / 2) + 
+    2 * p₁₁ * p₁₀ * (1 + ((p₀₁ / p₁₀) ^ .5) / 2) +
+    2 * p₁₀ * p₀₁ * (5 / 4 + ((p₀₁ / p₁₀) ^ .5 + (p₁₀ / p₀₁) ^ .5) / 2))
+    var_upper *= N
+    # variance terms
+    var_lower  = (p₁₁ * (1 − p₁₁) * (((p₁₁ ^ 4 + 3 * (p₁₀ ^ 2) * (p₀₁ ^ 2)) / ((p₁₁ ^ 2 + p₁₀ * p₀₁) ^ 2)) ^ 2) + 
+    p₀₁ * (1 − p₀₁) * ((1 + 2 * p₁₀ * (p₁₁ ^ 3) / ((p₁₁ ^ 2 + p₁₀ * p₀₁) ^ 2)) ^ 2) +
+    p₁₀ * (1 − p₁₀) * ((1 + 2 * p₀₁ * (p₁₁ ^ 3) / ((p₁₁ ^ 2 + p₁₀ * p₀₁) ^ 2)) ^ 2))
+    # covariance terms
+    var_lower -= 2 * (p₁₀ * p₀₁ * (1 + 2 * p₀₁ * (p₁₁ ^ 3) / ((p₁₁ ^ 2 + p₁₀ * p₀₁) ^ 2)) *
+    (1 + 2 * p₁₀ * (p₁₁ ^ 3) / ((p₁₁ ^ 2 + p₁₀ * p₀₁) ^ 2)) +
+    p₁₁ * p₁₀ * (1 + 2 * p₀₁ * (p₁₁ ^ 3) / ((p₁₁ ^ 2 + p₁₀ * p₀₁) ^ 2)) * 
+    ((p₁₁ ^ 4 + 3 * (p₁₀ ^ 2) * (p₀₁ ^ 2)) / ((p₁₁ ^ 2 + p₁₀ * p₀₁) ^ 2)) +
+    p₁₁ * p₀₁ * (1 + 2 * p₁₀ * (p₁₁ ^ 3) / ((p₁₁ ^ 2 + p₁₀ * p₀₁) ^ 2)) * 
+    ((p₁₁ ^ 4 + 3 * (p₁₀ ^ 2) * (p₀₁ ^ 2)) / ((p₁₁ ^ 2 + p₁₀ * p₀₁) ^ 2)))
+    var_lower *= N
+    
+    var_mean = (2 * p₁₁ * (1 − p₁₁) * (p₁₁ ^ 4 + 3 * (p₁₀ ^ 2) * (p₀₁ ^ 2)) / ((p₁₁ ^ 2 + p₁₀ * p₀₁) ^ 2) + 
+    2 * p₀₁ * (1 − p₀₁) * (1 + 2 * p₁₀ * (p₁₁ ^ 3) / ((p₁₁ ^ 2 + p₁₀ * p₀₁) ^ 2)) * (1 + ((p₀₁ / p₁₀) ^ .5) / 2) +
+    2 * p₁₀ * (1 − p₁₀) * (1 + 2 * p₀₁ * (p₁₁ ^ 3) / ((p₁₁ ^ 2 + p₁₀ * p₀₁) ^ 2)) * (1 + ((p₁₀ / p₀₁) ^ .5) / 2))
+    var_mean -= 2 * p₁₀ * p₀₁ * ((1 + ((p₁₀ / p₀₁) ^ .5) / 2) * 
+    (1 + 2 * p₁₀ * (p₁₁ ^ 3) / ((p₁₁ ^ 2 + p₁₀ * p₀₁) ^ 2)) + 
+    (1 + 2 * p₀₁ * (p₁₁ ^ 3) / ((p₁₁ ^ 2 + p₁₀ * p₀₁) ^ 2)) * 
+    (1 + ((p₀₁ / p₁₀) ^ .5) / 2))
+    var_mean -= 2 * p₁₁ * p₁₀ * (((p₁₁ ^ 4 + 3 * (p₁₀ ^ 2) * (p₀₁ ^ 2)) / ((p₁₁ ^ 2 + p₁₀ * p₀₁) ^ 2)) * 
+    (1 + ((p₁₀ / p₀₁) ^ .5) / 2) + (1 + 2 * p₀₁ * (p₁₁ ^ 3) / ((p₁₁ ^ 2 + p₁₀ * p₀₁) ^ 2)))
+    var_mean -= 2 * p₁₁ * p₀₁ * (((p₁₁ ^ 4 + 3 * (p₁₀ ^ 2) * (p₀₁ ^ 2)) / ((p₁₁ ^ 2 + p₁₀ * p₀₁) ^ 2)) * 
+    (1 + ((p₀₁ / p₁₀) ^ .5) / 2) + (1 + 2 * p₁₀ * (p₁₁ ^ 3) / ((p₁₁ ^ 2 + p₁₀ * p₀₁) ^ 2)))
+    
+    var_mean *= N / 4
+    var_mean += var_lower / 4 + var_upper / 4
+    
+    return (var_lower, var_mean, var_upper)
+end
+
+function variance_estimation(;N_est::Real, n₁₁::Real, 
+                             n₁₀::Real, n₀₁::Real)
+    p₀₀ = (N_est - (n₁₀ + n₀₁ + n₁₁)) / N_est
+    p₁₀ = n₁₀ / N_est
+    p₀₁ = n₀₁ / N_est
+    p₁₁ = n₁₁ / N_est
+    N   = N_est
+    
+    
+    var_upper  = p₁₁ * (1 - p₁₁) + p₀₁ * (1 - p₀₁) + p₁₀ * (1 - p₁₀)
+    var_upper += p₁₀ * (1 - p₁₀) * ((p₀₁ / p₁₀) ^ .5 + (p₀₁ / p₁₀) / 4)
+    var_upper += p₀₁ * (1 - p₀₁) * ((p₁₀ / p₀₁) ^ .5 + (p₁₀ / p₀₁) / 4)
+    # this is from covariance temrs
+    var_upper -= (2 * p₁₁ * p₀₁ * (1 + ((p₁₀ / p₀₁) ^ .5) / 2) + 
+    2 * p₁₁ * p₁₀ * (1 + ((p₀₁ / p₁₀) ^ .5) / 2) +
+    2 * p₁₀ * p₀₁ * (5 / 4 + ((p₀₁ / p₁₀) ^ .5 + (p₁₀ / p₀₁) ^ .5) / 2))
+    var_upper *= N
+    # variance terms
+    var_lower  = (p₁₁ * (1 − p₁₁) * (((p₁₁ ^ 4 + 3 * (p₁₀ ^ 2) * (p₀₁ ^ 2)) / ((p₁₁ ^ 2 + p₁₀ * p₀₁) ^ 2)) ^ 2) + 
+    p₀₁ * (1 − p₀₁) * ((1 + 2 * p₁₀ * (p₁₁ ^ 3) / ((p₁₁ ^ 2 + p₁₀ * p₀₁) ^ 2)) ^ 2) +
+    p₁₀ * (1 − p₁₀) * ((1 + 2 * p₀₁ * (p₁₁ ^ 3) / ((p₁₁ ^ 2 + p₁₀ * p₀₁) ^ 2)) ^ 2))
+    # covariance terms
+    var_lower -= 2 * (p₁₀ * p₀₁ * (1 + 2 * p₀₁ * (p₁₁ ^ 3) / ((p₁₁ ^ 2 + p₁₀ * p₀₁) ^ 2)) *
+    (1 + 2 * p₁₀ * (p₁₁ ^ 3) / ((p₁₁ ^ 2 + p₁₀ * p₀₁) ^ 2)) +
+    p₁₁ * p₁₀ * (1 + 2 * p₀₁ * (p₁₁ ^ 3) / ((p₁₁ ^ 2 + p₁₀ * p₀₁) ^ 2)) * 
+    ((p₁₁ ^ 4 + 3 * (p₁₀ ^ 2) * (p₀₁ ^ 2)) / ((p₁₁ ^ 2 + p₁₀ * p₀₁) ^ 2)) +
+    p₁₁ * p₀₁ * (1 + 2 * p₁₀ * (p₁₁ ^ 3) / ((p₁₁ ^ 2 + p₁₀ * p₀₁) ^ 2)) * 
+    ((p₁₁ ^ 4 + 3 * (p₁₀ ^ 2) * (p₀₁ ^ 2)) / ((p₁₁ ^ 2 + p₁₀ * p₀₁) ^ 2)))
+    var_lower *= N
+    
+    var_mean = (2 * p₁₁ * (1 − p₁₁) * (p₁₁ ^ 4 + 3 * (p₁₀ ^ 2) * (p₀₁ ^ 2)) / ((p₁₁ ^ 2 + p₁₀ * p₀₁) ^ 2) + 
+    2 * p₀₁ * (1 − p₀₁) * (1 + 2 * p₁₀ * (p₁₁ ^ 3) / ((p₁₁ ^ 2 + p₁₀ * p₀₁) ^ 2)) * (1 + ((p₀₁ / p₁₀) ^ .5) / 2) +
+    2 * p₁₀ * (1 − p₁₀) * (1 + 2 * p₀₁ * (p₁₁ ^ 3) / ((p₁₁ ^ 2 + p₁₀ * p₀₁) ^ 2)) * (1 + ((p₁₀ / p₀₁) ^ .5) / 2))
+    var_mean -= 2 * p₁₀ * p₀₁ * ((1 + ((p₁₀ / p₀₁) ^ .5) / 2) * 
+    (1 + 2 * p₁₀ * (p₁₁ ^ 3) / ((p₁₁ ^ 2 + p₁₀ * p₀₁) ^ 2)) + 
+    (1 + 2 * p₀₁ * (p₁₁ ^ 3) / ((p₁₁ ^ 2 + p₁₀ * p₀₁) ^ 2)) * 
+    (1 + ((p₀₁ / p₁₀) ^ .5) / 2))
+    var_mean -= 2 * p₁₁ * p₁₀ * (((p₁₁ ^ 4 + 3 * (p₁₀ ^ 2) * (p₀₁ ^ 2)) / ((p₁₁ ^ 2 + p₁₀ * p₀₁) ^ 2)) * 
+    (1 + ((p₁₀ / p₀₁) ^ .5) / 2) + (1 + 2 * p₀₁ * (p₁₁ ^ 3) / ((p₁₁ ^ 2 + p₁₀ * p₀₁) ^ 2)))
+    var_mean -= 2 * p₁₁ * p₀₁ * (((p₁₁ ^ 4 + 3 * (p₁₀ ^ 2) * (p₀₁ ^ 2)) / ((p₁₁ ^ 2 + p₁₀ * p₀₁) ^ 2)) * 
+    (1 + ((p₀₁ / p₁₀) ^ .5) / 2) + (1 + 2 * p₁₀ * (p₁₁ ^ 3) / ((p₁₁ ^ 2 + p₁₀ * p₀₁) ^ 2)))
+    
+    var_mean *= N / 4
+    var_mean += var_lower / 4 + var_upper / 4
+    
+    return (var_lower, var_mean, var_upper)
+end
+
+
+function sim_study(N, p1, p2, alpha)
+    tab = generate_counts(500, N, p1, p2, alpha, "-")
+    lb = (mapslices(x -> 2*x[2]*x[3]*x[1]/(x[2]*x[3]+x[1]^2), tab, dims = 1) .+ sum(tab[1:3,:], dims=1))
+    ub = (mapslices(x -> sqrt(x[2]*x[3]), tab, dims = 1) .+ sum(tab[1:3,:], dims=1))
+    me = (lb .+ ub) ./2
+    res = hcat(lb', me', ub')
+
+     table1 = vcat(mean(res, dims=1) .- N, var(res, dims=1))'
+
+    table2=hcat(
+        bias_approximation(N = N, p₁ = p1, p₂ = p2, α = alpha, dependence = "-", bias_type = "New") |> collect,
+        variance_approximation(N = N, p₁ = p1, p₂ = p2, α = alpha, dependence = "-") |> collect
+        )
+
+    res1=hcat(table1,table2)
+end
